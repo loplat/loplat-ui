9c975f38aee772408513e3ab5e6fc210ca89cf47
suyoungBae <mochapoke@loplat.com>
2021-08-31 16:55:27 +0900

    feature: palette 색상컬러 추가
    LL-13

---
+++ src/core/Palette/index.js
@@ -0,0 +1,37 @@
+// background color
+export const white = '#ffffff';
+export const gray_0 = '#f7f7f7';
+
+// primary
+export const lo_blue500 = '#41a7fd';
+export const lo_blue400 = '#5cb3fc';
+export const lo_blue300 = '#82c5fd';
+export const lo_blue200 = '#a8d8ff';
+export const lo_blue100 = '#cee9ff';
+
+export const lo_black500 = '#222222';
+export const lo_black400 = '#444444';
+export const lo_black300 = '#666666';
+export const lo_black200 = '#888888';
+export const lo_black100 = '#aaaaaa';
+
+// supporting color 1
+export const a_gray500 = '#9eaab7';
+export const a_gray400 = '#b8c4d1';
+export const a_gray300 = '#c8d3e0';
+export const a_gray200 = '#d5e0ed';
+export const a_gray100 = '#edf2f8';
+
+export const b_gray500 = '#b5b5b5';
+export const b_gray400 = '#c1c1c1';
+export const b_gray300 = '#d1d1d1';
+export const b_gray200 = '#e1e1e1';
+export const b_gray100 = '#f0f0f0';
+
+// supporting color 2
+export const red500 = '#d83a5e';
+export const red400 = '#ed6489';
+export const red300 = '#f47e9e';
+export const red200 = '#f59fb6';
+export const red100 = '#fac7d5';
+
