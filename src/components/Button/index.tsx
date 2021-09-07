488b5e937c009ba3b1e36c7fe61f3344a9da0bb4
devsangho <gaonmail@cau.ac.kr>
2021-09-07 12:27:55 +0900

    feat: button(without Icons)
    LL-16

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,228 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import {
+  background,
+  black300,
+  blue100,
+  blue200,
+  blue500,
+  blue600,
+  blueScale200,
+  blueScale400,
+  danger100,
+  danger300,
+  danger400,
+  grayScale200,
+  grayScale500,
+  white,
+} from '../../core/Palette';
+
+export type Size = 'sm' | 'md' | 'lg';
+export type Color = 'default' | 'primary1' | 'primary2' | 'danger1' | 'danger2' | 'solid';
+export interface ButtonProps {
+  size?: Size;
+  fullWidth?: boolean;
+  color?: Color;
+  leftIcon?: React.ReactElement;
+  rightIcon?: React.ReactElement;
+  disabled?: boolean;
+  className?: string;
+  onClick?: (event: React.MouseEvent<HTMLElement>) => void;
+  children?: React.ReactNode;
+}
+
+const SizeSet = {
+  fullWidth: {
+    width: '100%',
+    height: 'auto',
+  },
+  sm: {
+    width: '80px',
+    height: '38px',
+  },
+  md: {
+    width: '144px',
+    height: '54px',
+  },
+  lg: {
+    width: '192px',
+    height: '70px',
+  },
+} as const;
+
+const ColorSet = {
+  default: {
+    background: {
+      default: white,
+      hover: background,
+      act: white,
+      disabled: background,
+    },
+    border: {
+      default: grayScale200,
+      hover: blue500,
+      act: blue500,
+      disabled: grayScale200,
+    },
+    text: {
+      default: black300,
+      hover: black300,
+      act: blue500,
+      disabled: grayScale500,
+    },
+  },
+  primary1: {
+    background: {
+      default: blue500,
+      hover: blue600,
+      act: blue500,
+      disabled: blue100,
+    },
+    border: {
+      default: blue500,
+      hover: blue600,
+      act: blue500,
+      disabled: blue100,
+    },
+    text: {
+      default: white,
+      hover: white,
+      act: white,
+      disabled: blue200,
+    },
+  },
+  primary2: {
+    background: {
+      default: white,
+      hover: blue100,
+      act: blue500,
+      disabled: background,
+    },
+    border: {
+      default: blue500,
+      hover: blue500,
+      act: blue500,
+      disabled: grayScale200,
+    },
+    text: {
+      default: blue500,
+      hover: blue500,
+      act: white,
+      disabled: grayScale500,
+    },
+  },
+  danger1: {
+    background: {
+      default: danger300,
+      hover: danger400,
+      act: danger300,
+      disabled: danger100,
+    },
+    border: {
+      default: danger300,
+      hover: danger400,
+      act: danger300,
+      disabled: danger100,
+    },
+    text: {
+      default: white,
+      hover: white,
+      act: white,
+      disabled: `rgba(216, 58, 94, 0.4)`,
+    },
+  },
+  danger2: {
+    background: {
+      default: white,
+      hover: danger100,
+      act: danger300,
+      disabled: background,
+    },
+    border: {
+      default: danger300,
+      hover: danger300,
+      act: danger300,
+      disabled: grayScale200,
+    },
+    text: {
+      default: danger300,
+      hover: danger300,
+      act: white,
+      disabled: grayScale500,
+    },
+  },
+  solid: {
+    background: {
+      default: blueScale200,
+      hover: blueScale400,
+      act: blueScale200,
+      disabled: background,
+    },
+    border: {
+      default: blueScale200,
+      hover: blueScale400,
+      act: blueScale200,
+      disabled: background,
+    },
+    text: {
+      default: black300,
+      hover: black300,
+      act: black300,
+      disabled: grayScale500,
+    },
+  },
+} as const;
+
+const BaseButton = styled.button(
+  {
+    display: 'flex',
+    flexFlow: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    outline: 'none',
+    padding: '15px',
+    borderRadius: '4px',
+  },
+  (props: ButtonProps) => {
+    const { width, height } = SizeSet[props.fullWidth ? 'fullWidth' : props.size ?? 'sm'];
+    const { background, border, text } = ColorSet[props.color ?? 'default'];
+
+    return {
+      width,
+      height,
+      fontSize: '18px',
+      color: text.default,
+      backgroundColor: background.default,
+      border: `1px solid ${border.default}`,
+      '&:disabled': {
+        color: text.disabled,
+        backgroundColor: background.disabled,
+        borderColor: border.disabled,
+        cursor: 'not-allowed',
+      },
+      '&:hover:not(:disabled)': {
+        color: text.hover,
+        backgroundColor: background.hover,
+        borderColor: border.hover,
+      },
+      '&:active:not(:disabled)': {
+        color: text.act,
+        backgroundColor: background.act,
+        borderColor: border.act,
+        boxShadow: `inset 2px 2px 0 0 rgba(102, 102, 102, 0.2)`,
+      },
+    };
+  },
+);
+
+const Button = ({ disabled = false, ...props }: ButtonProps): React.ReactElement => {
+  return (
+    <BaseButton {...props} disabled={disabled}>
+      <div>{props.leftIcon}</div>
+      <div>{props.children}</div>
+      <div>{props.rightIcon}</div>
+    </BaseButton>
+  );
+};
+
+export default Button;
--- src/core/Palette/index.ts
+++ src/core/Palette/index.ts
@@ -14,20 +14,19 @@ export const black300 = '#666666';
 export const black400 = '#444444';
 export const black500 = '#222222';
 
-export const blueScale100 = '#EEF2F8'
-export const blueScale200 = '#D5E0EE'
-export const blueScale300 = '#C8D4E0'
-export const blueScale400 = '#B8C4D1'
-export const blueScale500 = '#9DAAB7'
+export const blueScale100 = '#EEF2F8';
+export const blueScale200 = '#D5E0EE';
+export const blueScale300 = '#C8D4E0';
+export const blueScale400 = '#B8C4D1';
+export const blueScale500 = '#9DAAB7';
 
-export const grayScale100 = '#F0F0F0'
-export const grayScale200 = '#E1E1E1'
-export const grayScale300 = '#D1D1D1'
-export const grayScale400 = '#5CB3FC'
-export const grayScale500 = '#B5B5B5'
+export const grayScale100 = '#F0F0F0';
+export const grayScale200 = '#E1E1E1';
+export const grayScale300 = '#D1D1D1';
+export const grayScale400 = '#5CB3FC';
+export const grayScale500 = '#B5B5B5';
 
 export const danger100 = '#F7D7DE';
 export const danger200 = '#E57A92';
 export const danger300 = '#d83a5e';
 export const danger400 = '#ba2547';
-
