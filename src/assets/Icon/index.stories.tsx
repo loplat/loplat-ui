584fcf170ab65f46fb762ebedad61bde8dd41acf
sanghoyoon <sanghoyoon@loplat.com>
2021-09-30 10:22:29 +0900

    feat: storybook에 모든 icon 추가

--- src/assets/Icon/index.stories.tsx
+++ src/assets/Icon/index.stories.tsx
@@ -2,18 +2,326 @@ import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { IconProps } from './index';
-import { Search } from './generated/Search';
+import {
+  AddIcon,
+  AlertIcon,
+  AppIcon,
+  ArrowDownIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ArrowUpIcon,
+  ArticleIcon,
+  BeatIcon,
+  BedtimeIcon,
+  BlockIcon,
+  BookIcon,
+  BookmarkIcon,
+  BoxAddIcon,
+  CachedIcon,
+  CalendarIcon,
+  CallIcon,
+  CallDisabledIcon,
+  CameraIcon,
+  CampaignIcon,
+  CancelcircleFillIcon,
+  CancelcircleOutlineIcon,
+  CardIcon,
+  ChartIcon,
+  ChartPieIcon,
+  CheckIcon,
+  CheckcircleFillIcon,
+  CheckcircleOutlineIcon,
+  CheveronDownIcon,
+  CheveronLeftIcon,
+  CheveronRightIcon,
+  CheveronUpIcon,
+  ClipIcon,
+  ClockIcon,
+  CloseIcon,
+  CloudIcon,
+  CloudDownloadIcon,
+  CloudUploadIcon,
+  CommandIcon,
+  CompassIcon,
+  CropIcon,
+  CrownIcon,
+  DeleteIcon,
+  DescriptionIcon,
+  DesktopIcon,
+  DiamondIcon,
+  DoneAllIcon,
+  DownloadIcon,
+  EditIcon,
+  EmojiEventIcon,
+  FeedIcon,
+  FileIcon,
+  FileAddIcon,
+  FilterIcon,
+  FireIcon,
+  FlagIcon,
+  GppIcon,
+  GppGoodIcon,
+  GpsIcon,
+  GridLineIcon,
+  GridViewIcon,
+  HeartIcon,
+  HeightIcon,
+  HomeIcon,
+  ImageIcon,
+  InboxIcon,
+  InfocircleFillIcon,
+  InfocircleOutlineIcon,
+  KeyIcon,
+  LanguageIcon,
+  LaptopIcon,
+  LaunchIcon,
+  LightningIcon,
+  LinkIcon,
+  ListIcon,
+  LockIcon,
+  LockOpenIcon,
+  MailIcon,
+  MapIcon,
+  MenuIcon,
+  MessageIcon,
+  MicOffIcon,
+  MicOnIcon,
+  MobileIcon,
+  MoneyIcon,
+  MoreHorizIcon,
+  MoreVertIcon,
+  MusicIcon,
+  NearMeIcon,
+  NotificationNewIcon,
+  NotificationNoneIcon,
+  PasteIcon,
+  PeopleAltIcon,
+  PersonAddIcon,
+  PlaceIcon,
+  PluscircleFillIcon,
+  PluscircleOutlineIcon,
+  PowerSettingIcon,
+  PresentIcon,
+  PrintIcon,
+  PushPinIcon,
+  RecOffIcon,
+  RecOnIcon,
+  RedoIcon,
+  RemoveFillIcon,
+  RemoveOutlineIcon,
+  ResetIcon,
+  RocketIcon,
+  SearchIcon,
+  SettingIcon,
+  ShareIcon,
+  ShieldIcon,
+  ShoppingBagIcon,
+  ShoppingCartIcon,
+  SignInIcon,
+  SignOutIcon,
+  StackIcon,
+  StarIcon,
+  StoreIcon,
+  StreetViewIcon,
+  SunIcon,
+  SyncIcon,
+  SyncAltIcon,
+  TagIcon,
+  TargetIcon,
+  TextIcon,
+  ThumbUpIcon,
+  TicketIcon,
+  TimerIcon,
+  TrashIcon,
+  TuneIcon,
+  UndoIcon,
+  UnfoldLessIcon,
+  UnfoldMoreIcon,
+  UploadIcon,
+  UserIcon,
+  UserCircleIcon,
+  VerifiedIcon,
+  VisibilityIcon,
+  VisibilityOffIcon,
+  VolumeOffIcon,
+  VolumeOffsetIcon,
+  VolumeOnIcon,
+  WalletIcon,
+  WarningCircleFillIcon,
+  WarningCircleOutlineIcon,
+  WatchIcon,
+  WaterIcon,
+  WaveIcon,
+  WidthIcon,
+  WifiIcon,
+  WriteIcon,
+  ZoomInIcon,
+  ZoomOutIcon,
+} from './index';
 
 const IconDocument = (props: IconProps) => {
   return (
     <>
-      <Search {...props} />
+      <AddIcon {...props} />
+      <AlertIcon {...props} />
+      <AppIcon {...props} />
+      <ArrowDownIcon {...props} />
+      <ArrowLeftIcon {...props} />
+      <ArrowRightIcon {...props} />
+      <ArrowUpIcon {...props} />
+      <ArticleIcon {...props} />
+      <BeatIcon {...props} />
+      <BedtimeIcon {...props} />
+      <BlockIcon {...props} />
+      <BookIcon {...props} />
+      <BookmarkIcon {...props} />
+      <BoxAddIcon {...props} />
+      <CachedIcon {...props} />
+      <CalendarIcon {...props} />
+      <CallIcon {...props} />
+      <CallDisabledIcon {...props} />
+      <CameraIcon {...props} />
+      <CampaignIcon {...props} />
+      <CancelcircleFillIcon {...props} />
+      <CancelcircleOutlineIcon {...props} />
+      <CardIcon {...props} />
+      <ChartIcon {...props} />
+      <ChartPieIcon {...props} />
+      <CheckIcon {...props} />
+      <CheckcircleFillIcon {...props} />
+      <CheckcircleOutlineIcon {...props} />
+      <CheveronDownIcon {...props} />
+      <CheveronLeftIcon {...props} />
+      <CheveronRightIcon {...props} />
+      <CheveronUpIcon {...props} />
+      <ClipIcon {...props} />
+      <ClockIcon {...props} />
+      <CloseIcon {...props} />
+      <CloudIcon {...props} />
+      <CloudDownloadIcon {...props} />
+      <CloudUploadIcon {...props} />
+      <CommandIcon {...props} />
+      <CompassIcon {...props} />
+      <CropIcon {...props} />
+      <CrownIcon {...props} />
+      <DeleteIcon {...props} />
+      <DescriptionIcon {...props} />
+      <DesktopIcon {...props} />
+      <DiamondIcon {...props} />
+      <DoneAllIcon {...props} />
+      <DownloadIcon {...props} />
+      <EditIcon {...props} />
+      <EmojiEventIcon {...props} />
+      <FeedIcon {...props} />
+      <FileIcon {...props} />
+      <FileAddIcon {...props} />
+      <FilterIcon {...props} />
+      <FireIcon {...props} />
+      <FlagIcon {...props} />
+      <GppIcon {...props} />
+      <GppGoodIcon {...props} />
+      <GpsIcon {...props} />
+      <GridLineIcon {...props} />
+      <GridViewIcon {...props} />
+      <HeartIcon {...props} />
+      <HeightIcon {...props} />
+      <HomeIcon {...props} />
+      <ImageIcon {...props} />
+      <InboxIcon {...props} />
+      <InfocircleFillIcon {...props} />
+      <InfocircleOutlineIcon {...props} />
+      <KeyIcon {...props} />
+      <LanguageIcon {...props} />
+      <LaptopIcon {...props} />
+      <LaunchIcon {...props} />
+      <LightningIcon {...props} />
+      <LinkIcon {...props} />
+      <ListIcon {...props} />
+      <LockIcon {...props} />
+      <LockOpenIcon {...props} />
+      <MailIcon {...props} />
+      <MapIcon {...props} />
+      <MenuIcon {...props} />
+      <MessageIcon {...props} />
+      <MicOffIcon {...props} />
+      <MicOnIcon {...props} />
+      <MobileIcon {...props} />
+      <MoneyIcon {...props} />
+      <MoreHorizIcon {...props} />
+      <MoreVertIcon {...props} />
+      <MusicIcon {...props} />
+      <NearMeIcon {...props} />
+      <NotificationNewIcon {...props} />
+      <NotificationNoneIcon {...props} />
+      <PasteIcon {...props} />
+      <PeopleAltIcon {...props} />
+      <PersonAddIcon {...props} />
+      <PlaceIcon {...props} />
+      <PluscircleFillIcon {...props} />
+      <PluscircleOutlineIcon {...props} />
+      <PowerSettingIcon {...props} />
+      <PresentIcon {...props} />
+      <PrintIcon {...props} />
+      <PushPinIcon {...props} />
+      <RecOffIcon {...props} />
+      <RecOnIcon {...props} />
+      <RedoIcon {...props} />
+      <RemoveFillIcon {...props} />
+      <RemoveOutlineIcon {...props} />
+      <ResetIcon {...props} />
+      <RocketIcon {...props} />
+      <SearchIcon {...props} />
+      <SettingIcon {...props} />
+      <ShareIcon {...props} />
+      <ShieldIcon {...props} />
+      <ShoppingBagIcon {...props} />
+      <ShoppingCartIcon {...props} />
+      <SignInIcon {...props} />
+      <SignOutIcon {...props} />
+      <StackIcon {...props} />
+      <StarIcon {...props} />
+      <StoreIcon {...props} />
+      <StreetViewIcon {...props} />
+      <SunIcon {...props} />
+      <SyncIcon {...props} />
+      <SyncAltIcon {...props} />
+      <TagIcon {...props} />
+      <TargetIcon {...props} />
+      <TextIcon {...props} />
+      <ThumbUpIcon {...props} />
+      <TicketIcon {...props} />
+      <TimerIcon {...props} />
+      <TrashIcon {...props} />
+      <TuneIcon {...props} />
+      <UndoIcon {...props} />
+      <UnfoldLessIcon {...props} />
+      <UnfoldMoreIcon {...props} />
+      <UploadIcon {...props} />
+      <UserIcon {...props} />
+      <UserCircleIcon {...props} />
+      <VerifiedIcon {...props} />
+      <VisibilityIcon {...props} />
+      <VisibilityOffIcon {...props} />
+      <VolumeOffIcon {...props} />
+      <VolumeOffsetIcon {...props} />
+      <VolumeOnIcon {...props} />
+      <WalletIcon {...props} />
+      <WarningCircleFillIcon {...props} />
+      <WarningCircleOutlineIcon {...props} />
+      <WatchIcon {...props} />
+      <WaterIcon {...props} />
+      <WaveIcon {...props} />
+      <WidthIcon {...props} />
+      <WifiIcon {...props} />
+      <WriteIcon {...props} />
+      <ZoomInIcon {...props} />
+      <ZoomOutIcon {...props} />
     </>
   );
 };
 
 export default {
-  title: 'Icon/Search',
+  title: 'Icon',
   component: IconDocument,
 } as ComponentMeta<typeof IconDocument>;
 
--- src/assets/Icon/tools/generate-export.js
+++ src/assets/Icon/tools/generate-export.js
@@ -4,21 +4,79 @@ const fs = require('fs');
 const path = require('path');
 
 const defaultPath = path.join(__dirname, '../');
-const iconFiles = fs.readdirSync(path.join(defaultPath, 'generated')).filter(file => file.includes('tsx'));
+const iconFiles = fs.readdirSync(path.join(defaultPath, 'generated')).filter((file) => file.includes('tsx'));
 
 const generatedCodes = [
   iconFiles
-    .map(iconFile => {
+    .map((iconFile) => {
       const fileName = iconFile.split('.')[0];
       return `export { ${fileName} as ${fileName}Icon } from './generated/${fileName}';`;
     })
     .join('\n'),
 ];
 
-fs.writeFile(path.join(defaultPath, 'export.generated.ts'), generatedCodes.join('\n'), error => {
+const generateIconFilesName = [
+  iconFiles
+    .map((iconFile) => {
+      const fileName = iconFile.split('.')[0];
+      return `${fileName}Icon`;
+    })
+    .join(', '),
+];
+
+const generateStorybook = [
+  iconFiles
+    .map((iconFile) => {
+      const fileName = iconFile.split('.')[0];
+      return `<${fileName}Icon {...props} />`;
+    })
+    .join('\n'),
+];
+
+fs.writeFile(path.join(defaultPath, 'export.generated.ts'), generatedCodes.join('\n'), (error) => {
   if (error) {
     throw error;
   }
 
   console.log('export.generated.ts has been saved!');
 });
+
+fs.writeFile(
+  path.join(defaultPath, 'index.stories.tsx'),
+  `
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { IconProps } from './index';
+import { ${generateIconFilesName} } from './index';
+
+const IconDocument = (props: IconProps) => {
+  return (
+    <>
+      ${generateStorybook}
+    </>
+  );
+};
+
+export default {
+  title: 'Icon/Search',
+  component: IconDocument,
+} as ComponentMeta<typeof IconDocument>;
+
+const Template: ComponentStory<typeof IconDocument> = (args: IconProps) => <IconDocument {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  size: 32,
+  fillColor: '#000000',
+};
+
+`,
+  (error) => {
+    if (error) {
+      throw error;
+    }
+
+    console.log('index.stories.tsx has been saved!');
+  },
+);
