4c6a7de4aa1ba9825d37e4de62f704b8b563ab32
sanghoyoon <sanghoyoon@loplat.com>
2021-10-14 14:36:23 +0900

    feat: Typo를 rem 단위로 변경, className prop 적용

--- src/core/Typography/Body/index.test.tsx
+++ src/core/Typography/Body/index.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { Body1, Body2, Body3, Body4 } from './index';
+import { Body1, Body2, Body3 } from './index';
 
 describe('<Body1 />', () => {
   test('<Body1 />에 텍스트가 나오는가?', () => {
@@ -21,14 +21,6 @@ describe('<Body2 />', () => {
 describe('<Body3 />', () => {
   test('<Body3 />에 텍스트가 나오는가?', () => {
     const { getByText } = render(<Body3>테스트</Body3>);
-    const label = getByText('테스트');
-    expect(label).toBeInTheDocument();
-  });
-});
-
-describe('<Body4 />', () => {
-  test('<Body4 />에 텍스트가 나오는가?', () => {
-    const { getByText } = render(<Body4>테스트</Body4>);
     const label = getByText('테스트');
     expect(label).toBeInTheDocument();
   });
