fc42da009bcceb20a0155c4077b526bf68393d1c
sanghoyoon <sanghoyoon@loplat.com>
2021-10-18 14:12:48 +0900

    fix: className이 undefined의 경우 처리

--- src/core/Typography/Body/index.tsx
+++ src/core/Typography/Body/index.tsx
@@ -7,7 +7,7 @@ export const Body1 = (props: TypographyProps): React.ReactElement => {
       className={`${TextElementStyle({
         ...props,
         size: 1,
-      })} ${props.className}`}
+      })} ${props.className ?? ''}`}
     >
       {props.children}
     </div>
@@ -20,7 +20,7 @@ export const Body2 = (props: TypographyProps): React.ReactElement => {
       className={`${TextElementStyle({
         ...props,
         size: 0.88,
-      })} ${props.className}`}
+      })} ${props.className ?? ''}`}
     >
       {props.children}
     </div>
@@ -33,7 +33,7 @@ export const Body3 = (props: TypographyProps): React.ReactElement => {
       className={`${TextElementStyle({
         ...props,
         size: 0.75,
-      })} ${props.className}`}
+      })} ${props.className ?? ''}`}
     >
       {props.children}
     </div>
