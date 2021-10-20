ce516404e8ea2d20cc87fe04a43b09ae9acd7678
sanghoyoon <sanghoyoon@loplat.com>
2021-10-20 10:32:15 +0900

    feat: row, col의 간격을 gap prop으로 지정할 수 있게 함.

--- src/core/layout/index.stories.tsx
+++ src/core/layout/index.stories.tsx
@@ -1,8 +1,8 @@
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import {Grid} from './Grid';
-import {Row} from './Row';
-import {Col} from './Col';
+import { Grid } from './Grid';
+import { Row } from './Row';
+import { Col } from './Col';
 import styled from '@emotion/styled';
 
 const StyledCol = styled(Col)`
@@ -15,6 +15,18 @@ const Layout = () => {
     <Grid>
       <Row>
         <StyledCol>12</StyledCol>
+      </Row>
+      <Row gap={4}>
+        <StyledCol span={6}>Row에 gap 4를 주었어요! 위, 아래로 2rem씩 간격을 갖습니다.</StyledCol>
+        <StyledCol span={6}>Row에 gap 4를 주었어요! 위, 아래로 2rem씩 간격을 갖습니다.</StyledCol>
+      </Row>
+      <Row>
+        <StyledCol span={6} gap={4}>
+          Col에 gap 4를 주었어요! 좌, 우로 2rem씩 간격을 갖습니다.
+        </StyledCol>
+        <StyledCol span={6} gap={4}>
+          Col에 gap 4를 주었어요! 좌, 우로 2rem씩 간격을 갖습니다.
+        </StyledCol>
       </Row>
       <Row>
         <StyledCol span={6}>6</StyledCol>
