31a6a687148e7ef45e6526d50c52a960c15d5c78
suyoungBae <mochapoke@loplat.com>
2021-11-02 16:06:03 +0900

    fix: cheveron -> chevron 오타 수정

--- src/assets/Icon/generated/ChevronDown.tsx
+++ src/assets/Icon/generated/ChevronDown.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import type { IconProps } from '../index';
-export const CheveronDown =
+export const ChevronDown =
   React.memo <
   IconProps >
   (({ size = 18, fillColor = '#9DAAB7', className, style }) => (
@@ -16,14 +16,14 @@ export const CheveronDown =
       <defs>
         <path
           d="M13.785 15.21c-.375 0-.734-.149-1-.414L.415 2.414a1.414 1.414 0 012-2l11.37 11.381L25.156.415a1.414 1.414 0 012 2l-12.37 12.381a1.417 1.417 0 01-1 .414z"
-          id="cheveron_Down_svg__a"
+          id="chevron_Down_svg__a"
         />
       </defs>
       <g transform="translate(2 8)" fill="none" fillRule="evenodd">
-        <mask id="cheveron_Down_svg__b" fill="#fff">
-          <use xlinkHref="#cheveron_Down_svg__a" />
+        <mask id="chevron_Down_svg__b" fill="#fff">
+          <use xlinkHref="#chevron_Down_svg__a" />
         </mask>
-        <g mask="url(#cheveron_Down_svg__b)">
+        <g mask="url(#chevron_Down_svg__b)">
           <path fill={fillColor} d="M-2-8h32v32H-2z" />
         </g>
       </g>
