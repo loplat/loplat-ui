e203a739b663a731fe0a81471176015b91e28ed5
jyp0808 <jyp0808@loplat.com>
2021-11-15 17:09:42 +0900

    chore: rollup build 시 code splitting 을 통해 용량 최적화

--- rollup.config.js
+++ rollup.config.js
@@ -7,28 +7,53 @@ import image from '@rollup/plugin-image';
 
 const packageJson = require('./package.json');
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: packageJson.main,
-      format: 'cjs',
-      sourcemap: true,
+export default [
+  {
+    // NOTE: 실제로 사용할 cjs 파일들을 code splitting 하여 빌드한다.
+    input: {
+      index: 'src/index.ts',
+      core: 'src/core/index.ts',
+      'components/index': 'src/components/index.ts',
+      'components/Button': 'src/components/Button/index.tsx',
     },
-    {
-      file: packageJson.module,
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    resolve(),
-    commonjs(),
-    typescript({ useTsconfigDeclarationDir: true }),
-    postcss({
-      extensions: ['.css'],
-    }),
-    image(),
-  ],
-};
+    output: [
+      {
+        dir: 'dist',
+        format: 'cjs',
+        sourcemap: true,
+        exports: 'auto',
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ useTsconfigDeclarationDir: true }),
+      postcss({
+        extensions: ['.css'],
+      }),
+      image(),
+    ],
+  },
+  {
+    // NOTE: autocomplete file path를 지원하기위해 esm 파일도 빌드한다.
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ useTsconfigDeclarationDir: true }),
+      postcss({
+        extensions: ['.css'],
+      }),
+      image(),
+    ],
+  },
+];
