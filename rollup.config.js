5abe965d24e7dd751ffbc83936b9e4b4421f13a8
jyp0808 <jyp0808@loplat.com>
2021-11-18 21:10:40 +0900

    chore: cjs 빌드 결과물을 없애고 esm 을 사용하도록 개선. tree shaking 기능 지원
    LL-76

--- rollup.config.js
+++ rollup.config.js
@@ -2,64 +2,33 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
+import multiInput from 'rollup-plugin-multi-input';
 import postcss from 'rollup-plugin-postcss';
 import image from '@rollup/plugin-image';
 import copy from 'rollup-plugin-copy';
 
-const packageJson = require('./package.json');
-
-export default [
-  {
-    // NOTE: 실제로 사용할 cjs 파일들을 code splitting 하여 빌드한다.
-    input: {
-      index: 'src/index.ts',
-      'assets/Icon/index': 'src/assets/Icon/index.ts',
-      'assets/Logo/index': 'src/assets/Logo/index.ts',
-      'core/index': 'src/core/index.ts',
-      'components/index': 'src/components/index.ts',
-      'components/Button/index': 'src/components/Button/index.tsx',
+export default {
+  // NOTE: tree shaking 을 위해 esm 파일들을 code splitting 하여 빌드한다.
+  input: ['src/**/index.ts', 'src/**/index.tsx', 'src/**/generated/*.tsx'],
+  output: [
+    {
+      dir: '.',
+      format: 'esm',
+      sourcemap: true,
     },
-    output: [
-      {
-        dir: '.',
-        format: 'cjs',
-        sourcemap: true,
-        exports: 'auto',
-      },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ useTsconfigDeclarationDir: true }),
-      postcss({
-        extensions: ['.css'],
-      }),
-      image(),
-      copy({
-        targets: [{ src: 'src/core/Palette/index.scss', dest: 'core/Palette' }],
-      }),
-    ],
-  },
-  {
-    // NOTE: autocomplete file path를 지원하기위해 esm 파일도 빌드한다.
-    input: 'src/index.ts',
-    output: [
-      {
-        file: packageJson.module,
-        format: 'esm',
-        sourcemap: true,
-      },
-    ],
-    plugins: [
-      peerDepsExternal(),
-      resolve(),
-      commonjs(),
-      typescript({ useTsconfigDeclarationDir: true }),
-      postcss({
-        extensions: ['.css'],
-      }),
-      image(),
-    ],
-  },
-];
+  ],
+  plugins: [
+    peerDepsExternal(),
+    multiInput(),
+    resolve(),
+    commonjs(),
+    typescript({ useTsconfigDeclarationDir: true }),
+    postcss({
+      extensions: ['.css'],
+    }),
+    image(),
+    copy({
+      targets: [{ src: 'src/core/Palette/index.scss', dest: 'core/Palette' }],
+    }),
+  ],
+};
