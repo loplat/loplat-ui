0742cc7feb82beb82d290d83ce366f7d718895c2
jyp0808 <jyp0808@loplat.com>
2021-12-02 17:40:47 +0900

    feat: margin, padding spacing 관련 type과 emotion css 추가
    LL-86

---
+++ src/core/Spacing/index.ts
@@ -0,0 +1,68 @@
+import { css } from '@emotion/react';
+import { SerializedStyles } from '@emotion/serialize';
+
+const STANDARD = 4;
+export const spacing = (operand: number): number => {
+  if (Number.isInteger(operand * 2)) {
+    // 0.5의 배수일 때
+    return STANDARD * operand;
+  } else {
+    // 0.5의 배수가 아닌 소수일 때
+    const integerPart = Math.floor(operand);
+    const fractionalPart = operand - integerPart;
+    const operandRoundedByOneHalf =
+      fractionalPart < 0.75 && fractionalPart > 0.25 ? integerPart + 0.5 : Math.round(operand);
+    return STANDARD * operandRoundedByOneHalf;
+  }
+};
+
+// Margin
+const marginSpacingOptions = ['mt', 'mb', 'ml', 'mr', 'my', 'mx'] as const;
+export type MarginSpacing = {
+  [key in typeof marginSpacingOptions[number]]?: number;
+};
+export const MarginSpacingProps = (props: MarginSpacing): MarginSpacing =>
+  marginSpacingOptions.reduce(
+    (prev, curr) => ({
+      ...prev,
+      [curr]: props[curr],
+    }),
+    {},
+  );
+export const MarginSpacingStyle = (props: MarginSpacing): SerializedStyles => css`
+  margin-top: ${spacing((props.mt || props.my) ?? 0)}px;
+  margin-bottom: ${spacing((props.mb || props.my) ?? 0)}px;
+  margin-left: ${spacing((props.ml || props.mx) ?? 0)}px;
+  margin-right: ${spacing((props.mr || props.mx) ?? 0)}px;
+`;
+
+// Padding
+const paddingSpacingOptions = ['pt', 'pb', 'pl', 'pr', 'py', 'px'] as const;
+export type PaddingSpacing = {
+  [key in typeof paddingSpacingOptions[number]]?: number;
+};
+export const PaddingSpacingProps = (props: PaddingSpacing): PaddingSpacing =>
+  paddingSpacingOptions.reduce(
+    (prev, curr) => ({
+      ...prev,
+      [curr]: props[curr],
+    }),
+    {},
+  );
+export const PaddingSpacingStyle = (props: PaddingSpacing): SerializedStyles => css`
+  padding-top: ${spacing((props.pt || props.py) ?? 0)}px;
+  padding-bottom: ${spacing((props.pb || props.py) ?? 0)}px;
+  padding-left: ${spacing((props.pl || props.px) ?? 0)}px;
+  padding-right: ${spacing((props.pr || props.px) ?? 0)}px;
+`;
+
+// Box(Margin + Padding)
+export type BoxSpacing = MarginSpacing & PaddingSpacing;
+export const BoxSpacingProps = (props: BoxSpacing): BoxSpacing => ({
+  ...MarginSpacingProps(props),
+  ...PaddingSpacingProps(props),
+});
+export const BoxSpacingStyle = (props: BoxSpacing): SerializedStyles => css`
+  ${MarginSpacingStyle(props)};
+  ${PaddingSpacingStyle(props)};
+`;
