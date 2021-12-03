26b82228acef05307797f12f81b07c187b4bfefa
jyp0808 <jyp0808@loplat.com>
2021-12-03 12:21:34 +0900

    feat: Typography font-size type 추가 및 적용

--- src/core/Typography/Body/index.tsx
+++ src/core/Typography/Body/index.tsx
@@ -1,41 +1,14 @@
 import React from 'react';
 import { TextElementStyle, TypographyProps } from '../BaseTypography';
 
-export const Body1 = React.memo((props: TypographyProps): React.ReactElement => {
+export const Body = (props: TypographyProps): React.ReactElement => {
   return (
     <div
       className={`${TextElementStyle({
         ...props,
-        size: 1,
       })} ${props.className ?? ''}`}
     >
       {props.children}
     </div>
   );
-});
-
-export const Body2 = React.memo((props: TypographyProps): React.ReactElement => {
-  return (
-    <div
-      className={`${TextElementStyle({
-        ...props,
-        size: 0.88,
-      })} ${props.className ?? ''}`}
-    >
-      {props.children}
-    </div>
-  );
-});
-
-export const Body3 = React.memo((props: TypographyProps): React.ReactElement => {
-  return (
-    <div
-      className={`${TextElementStyle({
-        ...props,
-        size: 0.75,
-      })} ${props.className ?? ''}`}
-    >
-      {props.children}
-    </div>
-  );
-});
+};
--- src/core/Typography/Headline/index.tsx
+++ src/core/Typography/Headline/index.tsx
@@ -1,80 +1,14 @@
 import React from 'react';
 import { TextElementStyle, TypographyProps } from '../BaseTypography';
 
-export const Headline1 = React.memo((props: TypographyProps): React.ReactElement => {
+export const Headline = (props: TypographyProps): React.ReactElement => {
   return (
     <h1
       className={`${TextElementStyle({
         ...props,
-        size: 2.5,
       })} ${props.className ?? ''}`}
     >
       {props.children}
     </h1>
   );
-});
-
-export const Headline2 = React.memo((props: TypographyProps): React.ReactElement => {
-  return (
-    <h2
-      className={`${TextElementStyle({
-        ...props,
-        size: 2,
-      })} ${props.className ?? ''}`}
-    >
-      {props.children}
-    </h2>
-  );
-});
-
-export const Headline3 = React.memo((props: TypographyProps): React.ReactElement => {
-  return (
-    <h3
-      className={`${TextElementStyle({
-        ...props,
-        size: 1.75,
-      })} ${props.className ?? ''}`}
-    >
-      {props.children}
-    </h3>
-  );
-});
-
-export const Headline4 = React.memo((props: TypographyProps): React.ReactElement => {
-  return (
-    <h4
-      className={`${TextElementStyle({
-        ...props,
-        size: 1.5,
-      })} ${props.className ?? ''}`}
-    >
-      {props.children}
-    </h4>
-  );
-});
-
-export const Headline5 = React.memo((props: TypographyProps): React.ReactElement => {
-  return (
-    <h5
-      className={`${TextElementStyle({
-        ...props,
-        size: 1.25,
-      })} ${props.className ?? ''}`}
-    >
-      {props.children}
-    </h5>
-  );
-});
-
-export const Headline6 = React.memo((props: TypographyProps): React.ReactElement => {
-  return (
-    <h6
-      className={`${TextElementStyle({
-        ...props,
-        size: 1.13,
-      })} ${props.className ?? ''}`}
-    >
-      {props.children}
-    </h6>
-  );
-});
+};
