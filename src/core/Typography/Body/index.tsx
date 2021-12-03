6dbdefd5c2bc0da29b3bc8a6c7e4c288cee5d7d3
jyp0808 <jyp0808@loplat.com>
2021-12-03 14:32:13 +0900

    feat: Typography에 tag prop 추가
    LL-54

--- src/core/Typography/Body/index.tsx
+++ src/core/Typography/Body/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { TextElementStyle, TypographyProps } from '../BaseTypography';
 
-export const Body = (props: TypographyProps): React.ReactElement => {
+type BodyProps = TypographyProps & {
+  tag?: 'p' | 'span';
+};
+
+export const Body = React.memo(({ tag = 'p', ...props }: BodyProps): React.ReactElement => {
+  const HTMLBodyElement = tag;
   return (
-    <div
+    <HTMLBodyElement
       className={`${TextElementStyle({
         ...props,
       })} ${props.className ?? ''}`}
     >
       {props.children}
-    </div>
+    </HTMLBodyElement>
   );
-};
+});
--- src/core/Typography/Caption/index.tsx
+++ src/core/Typography/Caption/index.tsx
@@ -3,13 +3,13 @@ import { TextElementStyle, TypographyProps } from '../BaseTypography';
 
 export const Caption = ({ size = '2xs', ...props }: TypographyProps): React.ReactElement => {
   return (
-    <div
+    <small
       className={`${TextElementStyle({
         ...props,
         size,
       })} ${props.className ?? ''}`}
     >
       {props.children}
-    </div>
+    </small>
   );
 };
