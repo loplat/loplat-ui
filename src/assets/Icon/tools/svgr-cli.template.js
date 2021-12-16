9f80ad4c1d1617a8e83c3c837d44bdeddd41e5c3
jyp0808 <jyp0808@loplat.com>
2021-12-16 15:26:05 +0900

    fix: uniqueId에 적용된 useMemo 삭제

--- src/assets/Icon/tools/svgr-cli.template.js
+++ src/assets/Icon/tools/svgr-cli.template.js
@@ -3,11 +3,11 @@ function defaultTemplate({ template }, _, { componentName, jsx }) {
   const IconComponentName = componentName.name.slice(3);
 
   return typeScriptTpl.ast`
-    import React, { useMemo } from 'react';
+    import React from 'react';
     import type { IconProps } from '../index';
     
     export const ${IconComponentName} = React.memo<IconProps>(({size = 18, fillColor = '#9DAAB7', suffixForId, className, style}) => {
-      const uniqueId = useMemo(() => suffixForId ?? String(Math.random().toString(36).slice(2, 11)), [suffixForId]);
+      const uniqueId = suffixForId ?? String(Math.random().toString(36).slice(2, 11));
       return ${jsx}
     })
   `;
