13eb5d3bf75b67c569ba598a9661deb35b771729
suyoungBae <mochapoke@loplat.com>
2021-12-23 11:32:42 +0900

    fix: storybook에서 Typography관련 size 옵션의 컨트롤이 object라서 테스트가 힘든 버그 수정
    LL-98

--- src/core/Typography/Headline/index.stories.tsx
+++ src/core/Typography/Headline/index.stories.tsx
@@ -3,25 +3,28 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Headline } from './index';
 import { TypographyProps } from '../BaseTypography';
+import { sizeProps } from '../BaseTypography/storyArgTypes';
 
 export default {
   title: 'Typography/Headline',
   component: Headline,
+  argTypes: {
+    ...sizeProps,
+  },
 } as ComponentMeta<typeof Headline>;
 
 const Template: ComponentStory<typeof Headline> = (args: TypographyProps) => (
   <>
-    <Headline {...args} tag="h3" size="xl" my={2}>
-      Headline 자율과 책임을 추구하는 location 전문 기업 로플랫
-    </Headline>
-    <Headline {...args} tag="h2" size="3xl" my={2}>
-      Headline 자율과 책임을 추구하는 location 전문 기업 로플랫
-    </Headline>
-    <Headline {...args} tag="h1" size="5xl" my={2}>
-      Headline 자율과 책임을 추구하는 location 전문 기업 로플랫
-    </Headline>
+    <Headline {...args}>자율과 책임을 추구하는 location 전문 기업 로플랫</Headline>
   </>
 );
 
 export const Default = Template.bind({});
 Default.args = {};
+
+export const BoldH3 = Template.bind({});
+BoldH3.args = {
+  textAlign: 'center',
+  tag: 'h3',
+  weight: 'bold',
+};
