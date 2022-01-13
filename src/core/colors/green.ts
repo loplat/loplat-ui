1123b4867a79a8aad614f858db32d7d151a4c310
ChangHyun2 <jchangh2@loplat.com>
2022-01-13 11:22:25 +0900

    colors, pallete

---
+++ src/core/colors/green.ts
@@ -0,0 +1,11 @@
+export const green100 = '#82c5fd';
+export const green200 = '#5cb3fc';
+export const green300 = '#40a6fd';
+export const green400 = '#529a2e';
+
+export const green = {
+  100: green100,
+  200: green200,
+  300: green300,
+  400: green400,
+} as const;
