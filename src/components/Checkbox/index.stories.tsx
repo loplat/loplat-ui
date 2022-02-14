ab1ef445ce6c142321775dac1776bede59581fd6
suyoungBae <mochapoke@loplat.com>
2022-02-14 16:45:04 +0900

    refactor: 라디오버튼, 체크박스 컴포넌트를 formContorl에서 컴포넌트 폴더로 이동
    LL-120

--- src/components/Checkbox/index.stories.tsx
+++ src/components/Checkbox/index.stories.tsx
@@ -3,7 +3,7 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Checkbox, CheckboxProps } from './index';
 
 export default {
-  title: 'formControls/Checkbox',
+  title: 'Components/Checkbox',
   component: Checkbox,
   argTypes: {
     checked: {
@@ -23,4 +23,19 @@ export const Default = Template.bind({});
 Default.args = {
   label: '체크박스',
   name: 'testBox',
+};
+
+export const Intermediate = Template.bind({});
+Intermediate.args = {
+  label: '중간값',
+  name: 'testBox',
+  checked: 'intermediate',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  label: '중간값 + disabled',
+  name: 'testBox',
+  checked: 'intermediate',
+  disabled: true,
 };
