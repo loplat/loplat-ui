df8ee4418192cb130ccf9ffd637576ce23127efa
jyp0808 <jyp0808@loplat.com>
2022-02-28 11:02:16 +0900

    feat(remove): Help 컴포넌트 삭제

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './Badge';
 export * from './Button';
 export * from './IconButton';
-export * from './Help';
 export * from './Input';
 export * from './Toast';
 export * from './Spinner';
