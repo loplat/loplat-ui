6b8dfb6354c44b0269d291940f92b4f7976ee5db
jyp0808 <jyp0808@loplat.com>
2022-03-10 19:44:31 +0900

    test: Button test 코드 수정, colors의 index.stories.tsx 이름 변경

--- src/components/Button/storybookTest.ts
+++ src/components/Button/storybookTest.ts
@@ -1,26 +1,25 @@
-import { Button, ButtonProps } from './';
+import { Button } from './';
 import { ComponentStory } from '@storybook/react';
 import { expect } from '@storybook/jest';
 import { userEvent, within } from '@storybook/testing-library';
 
 export function runTest(story: ComponentStory<typeof Button>): ComponentStory<typeof Button> {
-  story.play = async ({ args, canvasElement }: { args: ButtonProps; canvasElement: HTMLElement }) => {
+  story.play = async ({ args, canvasElement }) => {
     const canvas = within(canvasElement);
     const button = canvas.getByRole('button');
 
     // render children
     if (typeof args.children === 'string') {
-      await expect(button).toHaveTextContent(args.children);
+      expect(button).toHaveTextContent(args.children);
     }
 
     // click button
-    await userEvent.click(button);
-    await expect(args.onClick).toHaveBeenCalled();
-    await expect(button).toHaveFocus();
+    userEvent.click(button);
+    expect(args.onClick).toHaveBeenCalled();
 
     // click outside
-    await userEvent.click(canvasElement);
-    await expect(button).not.toHaveFocus();
+    userEvent.click(canvasElement);
+    expect(button).not.toHaveFocus();
   };
 
   return story;
