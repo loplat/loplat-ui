84f0de9d884f3a81e565bd411715da2f45e969de
suyoungBae <mochapoke@loplat.com>
2022-03-14 16:33:01 +0900

    chore: storybook-build fail fixed

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,6 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@storybook/preset-create-react-app',
     '@storybook/addon-interactions',
   ],
   features: {
