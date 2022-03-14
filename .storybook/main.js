45447cda5f241f686a44371b5eea85bf77ecb952
suyoungBae <mochapoke@loplat.com>
2022-03-14 19:54:00 +0900

    chore: storybook-build용 webpack, webpack-merge 삭제

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,3 @@
-const webpack = require('webpack');
-const { merge } = require('webpack-merge');
-
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)', './welcome.jsx'],
   addons: [
@@ -17,19 +14,13 @@ module.exports = {
     storyStoreV7: true,
   },
   webpackFinal: async (config) => {
-    return merge(config, {
-      optimization: {
-        splitChunks: {
-          chunks: 'all',
-          minSize: 30 * 1024,
-          maxSize: 244 * 1024,
-        },
-      },
-      performance: {
-        hints: false,
-        maxAssetSize: 244 * 1024,
-        maxEntrypointSize: 500 * 1024,
-      },
-    });
+    config.optimization.splitChunks = {
+      ...config.optimization.splitChunks,
+      chunks: 'all',
+      minSize: 30 * 1024,
+      maxSize: 244 * 1024,
+    };
+    config.performance = { ...config.performance, maxAssetSize: 500 * 1024, maxEntrypointSize: 500 * 1024 };
+    return config;
   },
 };
