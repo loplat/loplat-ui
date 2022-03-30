ed1a8c571eca0737541b4cc40fd1b6c5daf9b474
suyoungBae <mochapoke@loplat.com>
2022-03-30 10:40:53 +0900

    feat: Modal 컴포넌트에 접근성을 위한 로직 추가

--- src/utils/Modal/index.tsx
+++ src/utils/Modal/index.tsx
@@ -10,28 +10,38 @@ export interface ModalProps {
   children: React.ReactElement;
 }
 
+const TABBABLE_TAGS = ['a', 'input', 'select', 'button', 'textarea'];
+const changeAccessibility = (root: HTMLElement, isAccessible: boolean) => {
+  const tabIndex = isAccessible ? '0' : '-1';
+  const ariaHidden = (!isAccessible).toString();
+
+  root.setAttribute('aria-hidden', ariaHidden);
+  root.setAttribute('aria-disabled', ariaHidden);
+  for (const tabbableTag in TABBABLE_TAGS) {
+    const elements = root.getElementsByTagName(tabbableTag);
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].setAttribute('tabindex', tabIndex);
+    }
+  }
+};
+
 export function Modal({ isOpen, onClose, zIndex = 9999, children }: ModalProps): React.ReactElement | null {
   const portalId = useMemo(() => `loplat-ui-modal__${generateUniqueId()}`, []);
   const [container, setContainer] = useState<Element | null>(null);
 
   useEffect(() => {
-    // create container
     const newContainer = document.createElement('div');
     newContainer.setAttribute('id', portalId);
     document.body.appendChild(newContainer);
 
-    // add eventListeners
     function onKeyDownESC(e: KeyboardEvent) {
-      if (e.key === 'Escape') {
-        onClose();
-      }
+      if (e.key === 'Escape') onClose();
     }
     document.addEventListener('keydown', onKeyDownESC);
 
     // trigger rerender
     setContainer(newContainer);
 
-    // remove container & eventListeners
     return () => {
       document.removeEventListener('keydown', onKeyDownESC);
       const containerDOM = document.getElementById(portalId) as HTMLDivElement;
@@ -40,12 +50,16 @@ export function Modal({ isOpen, onClose, zIndex = 9999, children }: ModalProps):
   }, [portalId]);
 
   useEffect(() => {
+    // root div가 항상 body의 첫번째에 온다는 것을 전제로 상정함.
+    const root = document.body.querySelector('div') as HTMLElement;
+
     if (isOpen) {
-      // prevent scroll
       document.body.style.overflow = 'hidden';
+      changeAccessibility(root, false);
     }
     return () => {
-      document.body.style.overflow = '';
+      document.body.style.overflow = 'auto';
+      changeAccessibility(root, true);
     };
   }, [isOpen]);
 
@@ -79,5 +93,9 @@ const ModalWrapper = styled.div<Pick<ModalProps, 'zIndex'>>`
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 `;
