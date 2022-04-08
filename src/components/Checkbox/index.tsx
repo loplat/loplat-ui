7abacdc1a8a905d9c4f57ded876ab18606994831
suyoungBae <mochapoke@loplat.com>
2022-04-08 16:42:29 +0900

    feat: 체크박스에 margin spacing props 추가
    LL-154
    
    - 체크박스에 input 속성을 포괄적으로 추가
    - 스타일링 방식 수정
    - 체크박스 스토리북 문서 좀더 체계적으로 작성

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -3,40 +3,56 @@ import styled from '@emotion/styled';
 import { css } from '@emotion/react';
 import { blue500, grayscale800, grayscale200, grayscale500, grayscale100, grayscale300 } from '../../core/colors';
 import { primary } from '../../core/styles/palette';
+import { MarginSpacing, marginSpacingProps, marginSpacingStyle } from '../../core/Spacing';
 
 type BaseLabel = {
+  /** `label`의 폰트굵기를 굵게 만듭니다.*/
   boldLabel?: boolean;
   disabled?: boolean;
+  /** 체크 여부와 중간값을 할당할 수 있습니다.*/
   checked: boolean | 'checked' | 'unchecked' | 'intermediate';
 };
 
 export type CheckboxProps = {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  /** `label`과 `input`을 연동하는 속성입니다. form을 이용해 submit될 때, key값입니다.*/
   name: string;
+  /** 체크박스와 연동된 `label`태그입니다.*/
   label: string;
-  className?: string;
-} & BaseLabel;
+} & BaseLabel &
+  MarginSpacing &
+  Omit<React.InputHTMLAttributes<HTMLInputElement>, 'checked'>;
 
-export const Checkbox = ({ label, name, checked, onChange, disabled, ...props }: CheckboxProps): JSX.Element => {
+export const Checkbox = ({
+  label,
+  name,
+  checked,
+  onChange,
+  disabled,
+  className,
+  id,
+  ...props
+}: CheckboxProps): JSX.Element => {
   const isChecked: boolean = typeof checked === 'string' ? checked !== 'unchecked' : checked;
 
   return (
-    <Label checked={checked} disabled={disabled} {...props} data-testid="loplat-ui__label">
-      <input
-        data-testid="loplat-ui__checkbox"
-        type="checkbox"
-        name={name}
-        checked={isChecked}
-        onChange={onChange}
-        disabled={disabled}
-      />
-      <span />
+    <Label
+      checked={checked}
+      disabled={disabled}
+      className={className}
+      id={id}
+      {...marginSpacingProps(props)}
+      data-testid="loplat-ui__label"
+    >
+      <input type="checkbox" name={name} checked={isChecked} onChange={onChange} disabled={disabled} {...props} />
+      <Checkmark />
       {label}
     </Label>
   );
 };
 
-const Label = styled.label<BaseLabel>`
+const Label = styled.label<BaseLabel & MarginSpacing>`
+  ${marginSpacingStyle};
   width: fit-content;
   display: flex;
   flex-direction: row;
@@ -50,25 +66,10 @@ const Label = styled.label<BaseLabel>`
     position: absolute;
     opacity: 0;
 
-    & + span {
-      position: relative;
-      display: inline-block;
-      width: 1.25rem;
-      height: 1.25rem;
-      border: 1px solid ${grayscale200};
-      border-radius: 4px;
-      background-color: white;
-      margin-right: 0.5rem;
-    }
-    &:checked + span {
+    &:checked ~ span {
       border-color: ${blue500};
 
       &::after {
-        content: '';
-        position: absolute;
-        top: 50%;
-        left: 50%;
-
         ${(props) =>
           props.checked === 'intermediate'
             ? css`
@@ -89,19 +90,36 @@ const Label = styled.label<BaseLabel>`
               `};
       }
     }
-    &:disabled + span {
+    &:disabled ~ span {
       border-color: ${grayscale200};
       background-color: ${grayscale100};
-
       &::after {
         border-color: ${grayscale500};
       }
     }
-    &:hover:not(:checked) + span {
+    &:hover:not(:checked) ~ span {
       border-color: ${grayscale300};
     }
     &:focus-visible ~ span {
       outline: 2px solid ${primary};
     }
+  }
+`;
+
+const Checkmark = styled.span`
+  position: relative;
+  display: inline-block;
+  width: 1.25rem;
+  height: 1.25rem;
+  border: 1px solid ${grayscale200};
+  border-radius: 4px;
+  background-color: white;
+  margin-right: 0.5rem;
+
+  :after {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
   }
 `;
