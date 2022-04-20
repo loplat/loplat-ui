5e8782cabbad8dfd8fd5cc81f61993e323b754d4
suyoungBae <mochapoke@loplat.com>
2022-04-20 17:57:29 +0900

    test: 체크박스의 테스트를 storybookTest로 이관

--- src/components/Checkbox/storybookTest.ts
+++ src/components/Checkbox/storybookTest.ts
@@ -20,3 +20,36 @@ export function runTest(story: ComponentStory<typeof Checkbox>): ComponentStory<
 
   return story;
 }
+
+export function runGroupTest(story: ComponentStory<typeof Checkbox>): ComponentStory<typeof Checkbox> {
+  story.play = async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const checkFalse = canvas.getByLabelText('false');
+    const checkTrue = canvas.getByLabelText('true');
+    const checked = canvas.getByLabelText('checked');
+    const unchecked = canvas.getByLabelText('unchecked');
+    const intermediate = canvas.getByLabelText('intermediate');
+    const disabled = canvas.getAllByLabelText(/disabled/i);
+
+    expect(disabled[0]).toBeDisabled();
+
+    userEvent.click(checkFalse);
+    expect(checkFalse).toBeChecked();
+
+    userEvent.click(checkTrue);
+    expect(checkTrue).not.toBeChecked();
+
+    expect(checked).toBeChecked();
+    userEvent.click(checked);
+    expect(checked).not.toBeChecked();
+
+    expect(unchecked).not.toBeChecked();
+    userEvent.click(unchecked);
+    expect(unchecked).toBeChecked();
+
+    expect(intermediate).not.toBeChecked();
+    userEvent.click(intermediate);
+    expect(intermediate).toBeChecked();
+  };
+  return story;
+}
