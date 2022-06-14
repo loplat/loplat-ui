c7163021899ad284c3f6c1c90e4f0eab7bb3099b
jyp0808 <jyp0808@loplat.com>
2022-06-14 16:50:19 +0900

    feat: LineLoading 컴포넌트 추가
    LL-171

--- src/components/Loading/CircleLoading/index.tsx
+++ src/components/Loading/CircleLoading/index.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
+import type { Loading } from '../types';
 import { primary } from '../../../core';
-
-export type LoadingProps = {
-  /** 로딩 컴포넌트의 색상입니다. */
-  color?: string;
-  /** 로딩 컴포넌트가 한 바퀴 도는데 걸리는 시간을 `ms` 단위로 조절합니다. */
-  duration?: number;
-  /** 로딩 컴포넌트의 크기를 `px` 단위로 조절합니다. */
-  size?: number;
-  /** z-index를 결정합니다. */
-  zIndex?: number;
-};
-
-type Loading = LoadingProps & React.AriaAttributes;
 
 export const CircleLoading = ({
   color = primary,
@@ -22,7 +10,7 @@ export const CircleLoading = ({
   ...props
 }: Loading): React.ReactElement => {
   const originalSize = 40;
-  const scale = size / 40;
+  const scale = size / originalSize;
 
   return (
     <div
@@ -38,6 +26,7 @@ export const CircleLoading = ({
         height={`${originalSize}px`}
         viewBox={`0 0 ${originalSize} ${originalSize}`}
       >
+        {/* code from https://codepen.io/aurer/pen/ZEJxpO */}
         <path
           opacity="0.2"
           fill={color}
---
+++ src/components/Loading/LineLoading/index.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import type { Loading } from '../types';
+import { primary } from '../../../core';
+
+export const LineLoading = ({
+  color = primary,
+  duration = 600,
+  size = 80,
+  zIndex = 0,
+  ...props
+}: Loading): React.ReactElement => {
+  const originalSize = 30;
+  const scale = size / originalSize;
+
+  return (
+    <div
+      role="progressbar"
+      style={{ width: size, height: size, zIndex, display: 'flex', justifyContent: 'center', alignItems: 'center' }}
+      {...props}
+    >
+      <svg
+        style={{ transform: `scale(${scale})` }}
+        x="0px"
+        y="0px"
+        width={`${originalSize * 0.8}px`}
+        height={`${originalSize}px`}
+        viewBox={`0 0 ${originalSize * 0.8} ${originalSize}`}
+      >
+        {/* code from https://codepen.io/aurer/pen/ZEJxpO */}
+        <rect x="0" y="13" width="4" height="5" fill={color}>
+          <animate
+            attributeName="height"
+            attributeType="XML"
+            values="5;21;5"
+            begin="0s"
+            dur={`${duration}ms`}
+            repeatCount="indefinite"
+          />
+          <animate
+            attributeName="y"
+            attributeType="XML"
+            values="13; 5; 13"
+            begin="0s"
+            dur={`${duration}ms`}
+            repeatCount="indefinite"
+          />
+        </rect>
+        <rect x="10" y="13" width="4" height="5" fill={color}>
+          <animate
+            attributeName="height"
+            attributeType="XML"
+            values="5;21;5"
+            begin="0.15s"
+            dur={`${duration}ms`}
+            repeatCount="indefinite"
+          />
+          <animate
+            attributeName="y"
+            attributeType="XML"
+            values="13; 5; 13"
+            begin="0.15s"
+            dur={`${duration}ms`}
+            repeatCount="indefinite"
+          />
+        </rect>
+        <rect x="20" y="13" width="4" height="5" fill={color}>
+          <animate
+            attributeName="height"
+            attributeType="XML"
+            values="5;21;5"
+            begin="0.3s"
+            dur={`${duration}ms`}
+            repeatCount="indefinite"
+          />
+          <animate
+            attributeName="y"
+            attributeType="XML"
+            values="13; 5; 13"
+            begin="0.3s"
+            dur={`${duration}ms`}
+            repeatCount="indefinite"
+          />
+        </rect>
+      </svg>
+    </div>
+  );
+};
