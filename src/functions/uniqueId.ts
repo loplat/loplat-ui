ff5e90ba0517958d52415a0e712ec9721c701a78
jyp0808 <jyp0808@loplat.com>
2022-07-25 10:27:54 +0900

    fix: 불필요한 uniqueId 관련 함수 삭제

--- src/functions/uniqueId.ts
+++ src/functions/uniqueId.ts
@@ -1,11 +1,3 @@
 export function generateUniqueId(): string {
   return '_' + String(Math.random().toString(36).slice(2, 11));
 }
-
-export function attachUniqueId(value: string, uniqueId: string): string {
-  return value + uniqueId;
-}
-
-export function detachUniqueId(valueWithUniqueId: string, uniqueId: string): string {
-  return valueWithUniqueId.split(uniqueId)[0];
-}
