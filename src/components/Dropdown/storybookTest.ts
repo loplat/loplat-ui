0c8a4948a04b2bf28edca84652f36ca508b9bb2e
jyp0808 <jyp0808@loplat.com>
2022-08-01 16:21:45 +0900

    test: Dropdown Test code 수정

--- src/components/Dropdown/storybookTest.ts
+++ src/components/Dropdown/storybookTest.ts
@@ -5,21 +5,19 @@ import { userEvent, screen, within } from '@storybook/testing-library';
 import { sleep } from '../../functions/delay';
 
 export function runTest(story: ComponentStory<typeof Dropdown>): ComponentStory<typeof Dropdown> {
+  const options = ['loplat X', 'loplat i', 'WPS', 'IPS'];
   const delay = 500;
 
   story.play = async ({ args, canvasElement }) => {
     const canvas = within(canvasElement);
     const bar = canvas.getByRole('button');
 
-    // placeholder
-    expect(bar.textContent).toBe(args.placeholder);
-
     // click bar & menu
     userEvent.click(bar);
     const menus = screen.getAllByRole('option');
     await sleep(delay);
     userEvent.click(menus[3]);
-    expect(bar.textContent).toBe(args.options[3].value);
+    expect(bar.textContent).toBe(options[3]);
 
     // keyboard interactions
     await sleep(delay);
@@ -28,8 +26,8 @@ export function runTest(story: ComponentStory<typeof Dropdown>): ComponentStory<
     expect(bar.ariaExpanded).toBe('true');
 
     await sleep(delay);
-    userEvent.keyboard('{arrowup}{arrowup}{arrowup}{arrowup}{enter}');
-    expect(bar.textContent).toBe(args.options[0].value);
+    userEvent.keyboard('{arrowup}{arrowup}{arrowup}{enter}');
+    expect(bar.textContent).toBe(options[0]);
     expect(bar.ariaExpanded).toBe('false');
 
     userEvent.click(canvasElement);
