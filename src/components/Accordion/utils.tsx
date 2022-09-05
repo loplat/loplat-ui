9747fb0678ac368e253b0556157b940562e81d1d
suyoungBae <mochapoke@loplat.com>
2022-09-05 14:41:41 +0900

    fix: 아코디언의 상태를 바깥에서 주입해도 toggle animation이 잘 되어야함
    fix: web-animations-js 폴리필 dependencies에 추가 작성

--- src/components/Accordion/utils.tsx
+++ src/components/Accordion/utils.tsx
@@ -12,6 +12,7 @@ export const AccordionContextProvider = ({
   easing,
   accordionRef,
 }: AccordionContextProviderProps): JSX.Element => {
+  const isMounted = useRef<boolean>(false);
   const animationStatus = useRef<{ expanding: boolean; shrinking: boolean }>({ expanding: false, shrinking: false });
   const contentRef = useRef<HTMLDivElement>(null);
   const accordionAnimationRef = useRef<Animation | null>(null);
@@ -28,64 +29,75 @@ export const AccordionContextProvider = ({
     import('web-animations-js');
   }, []);
 
-  const toggle = (e: MouseEvent<HTMLElement>) => {
-    const accordion = accordionRef.current;
-    const content = contentRef.current;
-    if (!accordion) throw new DOMException('AccordionTitle must be used within Accordion.', 'NotFoundError');
-    if (!content) throw new DOMException('AccordionBody must be used within Accordion.', 'NotFoundError');
-
-    if (onToggle) onToggle(e);
-
-    accordion.classList.toggle('expanded');
-    const isExpanded = accordion.hasAttribute('open');
-    const { expanding, shrinking } = animationStatus.current;
-    const iconAnimation = iconAnimationRef.current;
-    const currentHeight = content.offsetHeight;
-    if (shrinking || !isExpanded) {
-      requestAnimationFrame(() => {
-        animateOpen(currentHeight);
-        if (iconAnimation) {
-          iconAnimation.playbackRate = 1;
-          iconAnimation.play();
-        }
-      });
-    } else if (expanding || isExpanded) {
-      requestAnimationFrame(() => {
-        setAnimation('shrinking', currentHeight, 0);
-        if (iconAnimation) {
-          iconAnimation.playbackRate = -1;
-          iconAnimation.play();
-        }
-      });
-    }
-  };
-
   useEffect(() => {
     // NOTE: open=true가 초기값일 경우, CLS가 생기지 않도록 하는 로직
     if (initialOpen) {
-      const content = contentRef.current;
-      const accordion = accordionRef.current;
-      if (!accordion) throw new DOMException('Accordion must be used.', 'NotFoundError');
-      if (!content) throw new DOMException('AccordionBody must be used within Accordion.', 'NotFoundError');
-
+      const { accordion, content } = domRequirements();
       accordion.setAttribute('open', 'true');
       accordion.classList.add('expanded');
       content.style.height = 'auto';
     }
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const animateOpen = (currentHeight: number) => {
-    const content = contentRef.current;
-    const accordion = accordionRef.current;
-    if (!accordion) throw new DOMException('Accordion must be used.', 'NotFoundError');
-    if (!content) throw new DOMException('AccordionBody must be used within Accordion.', 'NotFoundError');
+  useEffect(() => {
+    if (!isMounted.current) {
+      isMounted.current = true;
+      return;
+    }
+    animateAccordionByStatus(initialOpen);
+  }, [initialOpen]); // eslint-disable-line react-hooks/exhaustive-deps
 
+  const domRequirements = () => {
+    const accordion = accordionRef.current;
+    const content = contentRef.current;
+    if (!accordion) throw new DOMException('AccordionTitle must be used within Accordion.', 'NotFoundError');
+    if (!content) throw new DOMException('AccordionBody must be used within Accordion.', 'NotFoundError');
+    return { accordion, content };
+  };
+
+  const toggle = (e: MouseEvent<HTMLElement>) => {
+    if (onToggle) return onToggle(e);
+    const { accordion } = domRequirements();
+    const isExpanded = accordion.hasAttribute('open');
+    const { expanding, shrinking } = animationStatus.current;
+    if (shrinking || !isExpanded) {
+      requestAnimationFrame(() => {
+        animateAccordionByStatus(true);
+      });
+    } else if (expanding || isExpanded) {
+      requestAnimationFrame(() => {
+        animateAccordionByStatus(false);
+      });
+    }
+  };
+
+  const animateAccordionByStatus = (isOpen: boolean) => {
+    const { content } = domRequirements();
+    const iconAnimation = iconAnimationRef.current;
+    const currentHeight = content.offsetHeight;
+    if (isOpen) {
+      animateOpen(currentHeight);
+      if (iconAnimation) {
+        iconAnimation.playbackRate = 1;
+        iconAnimation.play();
+      }
+    } else {
+      setAccordionAnimation('shrinking', currentHeight, 0);
+      if (iconAnimation) {
+        iconAnimation.playbackRate = -1;
+        iconAnimation.play();
+      }
+    }
+  };
+
+  const animateOpen = (currentHeight: number) => {
+    const { accordion, content } = domRequirements();
     // NOTE: safari에서 Body div(height=0) 일때, 자식의 offsetHeight을 0으로 계산하는 이슈 대응
     accordion.setAttribute('open', 'true');
     content.style.height = 'auto';
     const finalHeight = content.scrollHeight;
     content.style.height = `${currentHeight}px`;
-    setAnimation('expanding', currentHeight, finalHeight);
+    setAccordionAnimation('expanding', currentHeight, finalHeight);
   };
 
   const setIconAnimation = (keyframes: Keyframe[], iconRef: RefObject<HTMLDivElement>): void => {
@@ -98,10 +110,19 @@ export const AccordionContextProvider = ({
     iconAnimationRef.current = iconAnimation;
   };
 
-  const setAnimation = (status: 'expanding' | 'shrinking', start: number, end: number) => {
-    const content = contentRef.current;
-    if (!content) throw new DOMException('Accordion.Body must be used within Accordion.', 'NotFoundError');
-
+  const setAccordionAnimation = (status: 'expanding' | 'shrinking', start: number, end: number) => {
+    const { accordion, content } = domRequirements();
+    switch (status) {
+      case 'expanding':
+        if (accordion.classList.contains('expanded')) return;
+        accordion.classList.add('expanded');
+        break;
+      case 'shrinking':
+        accordion.classList.remove('expanded');
+        break;
+      default:
+        break;
+    }
     animationStatus.current[status] = true;
 
     if (accordionAnimationRef.current) accordionAnimationRef.current.cancel();
@@ -119,14 +140,10 @@ export const AccordionContextProvider = ({
   };
 
   const onAnimationFinish = (status: 'expanding' | 'shrinking') => {
-    const accordion = accordionRef.current;
-    const content = contentRef.current;
-    if (!accordion) throw new DOMException('Accordion must be used.', 'NotFoundError');
-    if (!content) throw new DOMException('AccordionBody must be used within Accordion.', 'NotFoundError');
+    const { accordion, content } = domRequirements();
 
     /** NOTE: details 의 자식 중 summary 이외는 DOM에서 즉각적으로 숨기기 때문에,
-     * 닫을 때는 애니메이션이 완전히 끝난 후 open 상태값을 변경해야 함
-     */
+     * 닫을 때는 애니메이션이 완전히 끝난 후 open 상태값을 변경해야 함 */
     accordion.open = status === 'expanding';
 
     // NOTE: height을 auto로 변경하여, screen resizing 대응
