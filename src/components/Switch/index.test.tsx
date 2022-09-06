46c35070e41756a7cfc1fe9eeb7b9d0783dca2b8
suyoungBae <mochapoke@loplat.com>
2022-09-06 16:18:22 +0900

    test: Switch의 disabled 기능 테스트 코드 추가

--- src/components/Switch/index.test.tsx
+++ src/components/Switch/index.test.tsx
@@ -3,7 +3,11 @@ import { render } from '@testing-library/react';
 import { Switch } from './index';
 import userEvent from '@testing-library/user-event';
 
-const TestWrapper = ({ checked = false }: { checked?: boolean }) => {
+type Props = {
+  checked?: boolean;
+  disabled?: boolean;
+};
+const TestWrapper = ({ checked = false, disabled = false }: Props) => {
   const [isChecked, setChecked] = useState<boolean>(checked);
   return (
     <Switch
@@ -12,6 +16,7 @@ const TestWrapper = ({ checked = false }: { checked?: boolean }) => {
         setChecked((prev) => !prev);
       }}
       variant="outlined"
+      disabled={disabled}
     />
   );
 };
@@ -31,7 +36,7 @@ describe('<Switch/>', () => {
     userEvent.click(checkbox);
     expect(checkbox).not.toBeChecked();
   });
-  test('최초값이 적용되어야 한다', () => {
+  test('최초값이 false일 경우 그 값이 적용되어야 한다', () => {
     const { getByRole } = render(<TestWrapper checked={true} />);
     const checkbox = getByRole('checkbox');
     expect(checkbox).toBeChecked();
@@ -42,6 +47,14 @@ describe('<Switch/>', () => {
     const { getByRole } = render(<TestWrapper checked={false} />);
     const checkbox = getByRole('checkbox');
     expect(checkbox).not.toBeChecked();
+    userEvent.click(checkbox);
+    expect(checkbox).toBeChecked();
+  });
+  test('최초값이 true이고 disabled일 때 값이 변경되지 않아야한다', () => {
+    const { getByRole } = render(<TestWrapper checked={true} disabled={true} />);
+    const checkbox = getByRole('checkbox');
+    expect(checkbox).toBeDisabled();
+    expect(checkbox).toBeChecked();
     userEvent.click(checkbox);
     expect(checkbox).toBeChecked();
   });
