e902cbe61ed734d9fadfd2cbda0f58026deb0226
suyoungBae <mochapoke@loplat.com>
2023-04-25 19:17:22 +0900

    feat: Badge 컴포넌트 스타일 수정
    LL-225

--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,63 +1,82 @@
 import React from 'react';
-import { css } from '@emotion/css';
-import { white } from '../../core/colors';
-import { primary, danger, warning, success, primaryLight } from '../../core/styles/palette';
-import { BoxSpacing, boxSpacingStyle } from '../../core/Spacing';
+import styled from '@emotion/styled';
+import {
+  background300,
+  black,
+  bluescale400,
+  bluescale500,
+  grayscale700,
+  teal,
+  transparent,
+  white,
+} from '../../core/colors';
+import { primary, danger } from '../../core/styles/palette';
+import { MarginSpacing, marginSpacingProps, marginSpacingStyle } from '../../core/Spacing';
 
-type BadgeType = 'pill' | 'round' | 'new';
-type Color = 'primary' | 'danger' | 'success' | 'light' | 'warning';
-type variant = 'filled' | 'outlined';
-type Size = 'normal' | 'small';
+type Variant =
+  | 'primary1'
+  | 'primary2'
+  | 'success1'
+  | 'success2'
+  | 'danger1'
+  | 'danger2'
+  | 'light1'
+  | 'light2'
+  | 'done1'
+  | 'done2'
+  | 'selected';
+type Size = 'small' | 'large';
 
-export type BadgeProps = {
-  type?: BadgeType;
-  color?: Color;
-  text: string;
-  variant?: variant;
+export type BadgeBaseProps = {
+  variant?: Variant;
   size?: Size;
-} & BoxSpacing &
-  React.HTMLAttributes<HTMLDivElement>;
+};
 
-const ColorSet = {
-  primary,
-  light: primaryLight,
-  danger,
-  success,
-  warning,
-} as const;
+export type BadgeProps = BadgeBaseProps & MarginSpacing & React.HTMLAttributes<HTMLDivElement>;
 
-export const Badge = ({
-  type = 'pill',
-  color = 'primary',
-  text,
-  variant = 'filled',
-  size = 'normal',
-  className,
-  ...props
-}: BadgeProps): React.ReactElement => {
-  const [padding, fontSize] =
-    type === 'new' ? [0, '0.5rem'] : size === 'small' ? ['1px 5px', '0.5rem'] : ['3px 11px', '0.75rem'];
-  const [fontColor, backgroundColor] = variant === 'filled' ? [white, ColorSet[color]] : [ColorSet[color], white];
-
+export const Badge = ({ size = 'small', variant = 'primary1', children, ...props }: BadgeProps): React.ReactElement => {
   return (
-    <div
-      className={`${css`
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        ${type === 'new' ? `width: 16px; height: 16px;` : `width: fit-content;`};
-        padding: ${padding};
-        font-size: ${fontSize};
-        box-sizing: border-box;
-        border-radius: ${type === 'new' ? '100%' : type === 'pill' ? '12px' : '4px'};
-        border: 1px solid ${ColorSet[color]};
-        color: ${fontColor};
-        background-color: ${backgroundColor};
-        ${boxSpacingStyle(props)};
-      `} ${className}`}
-      {...props}
-    >
-      {type === 'new' ? text.charAt(0) : text}
-    </div>
+    <BadgeWrapper variant={variant} size={size} {...marginSpacingProps(props)} {...props}>
+      {children}
+    </BadgeWrapper>
   );
 };
+
+const BADGE_STYLE: Record<Size, { padding: string; minWidth: string }> = {
+  small: { padding: '4px 8px', minWidth: '68px' },
+  large: { padding: '8px', minWidth: '104px' },
+};
+type StylingProps = 'background' | 'border' | 'text';
+const BADGE_PALETTE: Record<Variant, Record<StylingProps, string>> = {
+  primary1: { background: primary, border: primary, text: white },
+  primary2: { background: transparent, border: primary, text: primary },
+  success1: { background: teal, border: teal, text: white },
+  success2: { background: transparent, border: teal, text: teal },
+  danger1: { background: danger, border: danger, text: white },
+  danger2: { background: transparent, border: danger, text: danger },
+  light1: { background: background300, border: background300, text: grayscale700 },
+  light2: { background: transparent, border: transparent, text: bluescale500 },
+  done1: { background: bluescale400, border: bluescale400, text: white },
+  done2: { background: transparent, border: bluescale500, text: bluescale500 },
+  selected: { background: white, border: primary, text: black },
+};
+
+const BadgeWrapper = styled.div<Required<BadgeBaseProps> & MarginSpacing>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.75rem;
+  line-height: 1.125rem;
+  box-sizing: border-box;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  width: max-content;
+  ${({ size }) => `padding: ${BADGE_STYLE[size].padding}; min-width: ${BADGE_STYLE[size].minWidth}`};
+  ${({ variant }) =>
+    `border-color: ${BADGE_PALETTE[variant].border}; background: ${BADGE_PALETTE[variant].background}; color: ${BADGE_PALETTE[variant].text}`}
+  ${marginSpacingStyle};
+
+  svg g path {
+    fill: ${({ variant }) => BADGE_PALETTE[variant].text};
+  }
+`;
